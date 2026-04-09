de285194b4589b055a44bb8fa4a5745433f689e9
Franco Leyes <leyes404@gmail.com>
2026-04-09 03:29:07 -0300

    [IMP] chatroom: add icon.svg and set manifest icon path

--- chatroom/__manifest__.py
+++ chatroom/__manifest__.py
@@ -5,6 +5,7 @@
     "summary": "Manage customer conversations and chat rooms",
     "author": "Nowi",
     "website": "https://nowi.com.ar",
+    "icon": "/chatroom/static/description/icon.svg",
     "license": "LGPL-3",
     "depends": [
         "base",
